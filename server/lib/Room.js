19f484f1f76baf39272d710dd93710ad7ddd5736
Dmitry Zvada <dmitryzvada@gmail.com>
2020-09-24 18:32:30 +0300

    remove unpause for broadcast

--- server/lib/Room.js
+++ server/lib/Room.js
@@ -1405,9 +1405,9 @@ class Room extends EventEmitter {
 
             // Now that we got the positive response from the remote Peer and, if
             // video, resume the Consumer to ask for an efficient key frame.
-            if (broadcast) {
-                await consumer.resume();
-            }
+            // if (broadcast) {
+            //     await consumer.resume();
+            // }
 
             // this._notification(
             // 	consumerPeer.socket,
